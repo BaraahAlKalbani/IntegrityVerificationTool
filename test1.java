3ab59bd2c2fbf6ed328331e89f8f85b07cf1683b
Bara'ah AL-Kalbani <baraalkalbani@gmail.com>
2022-12-07 00:15:26 -0800

    Added a Hellow world Program

---
+++ test1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class test1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Testing Output");
+		System.out.println("Testing Input");
+		String input=sc.nextLine();
+		
+		
+		
+	}
+}
